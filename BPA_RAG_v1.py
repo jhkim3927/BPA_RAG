75b933fb9c64285007f44a7435f80de788d459f7
jhkim3927 <jhkim3927@postech.ac.kr>
2026-09-22 08:51:53 +0900

    inital commit

--- BPA_RAG_v1.py
+++ BPA_RAG_v1.py
@@ -17,16 +17,3 @@ print("\n" + "=" * 70)
 
 """
 
-import subprocess
-import sys
-
-subprocess.check_call([
-    sys.executable, "-m", "pip", "install",
-    "numpy==1.26.4",
-    "langchain",
-    "langchain-community",
-    "langchain-ollama",
-    "chromadb",
-    "sentence-transformers",
-    "pypdf"
-])
